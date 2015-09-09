49444d57e5a9ff97087472ad5a668e9bdfdd5ec6
Elvis Martins <elvisbmartins@gmail.com>
2015-09-09 17:00:57 -0300

    template page-courses criado

--- src/wp-content/themes/timtec/templates/page-courses.php
+++ src/wp-content/themes/timtec/templates/page-courses.php
@@ -1,4 +1,129 @@
+<?php get_template_part('templates/head'); ?>
+
 <?php
+    do_action('get_header');
+    get_template_part('templates/header');
+?>
 
+<section id="banner" class=" ">
+	<div class="container">
+		<div class="col-md-6 text">
+			<h4 >
+				<?php  _oi("Lorem ipsum dolor sit amet, consectetur adipiscing elit."); ?>
+			</h4>   
+		</div>
+		<div class="col-md-6 image">
+			<img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/bannerv1.png" />
+		</div>
+	</div>
+</section>
 
-echo __FILE__;
+<section id="section1" class="">
+	<div class="container">
+		<div class="row text">
+			<h2>
+				<?php  _oi("Lorem ipsum dolor sit amet"); ?>
+			</h2>
+			<p><?php  _oi("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+				id velit lobortis, ultricies magna porta, molestie diam."); ?></p>
+		</div>
+		<div class="boxs row">
+			<div class="col-md-4">
+				<div class="box">
+					<div class="icon">
+						<img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/icon_cursos.png" />
+					</div>
+					<div class="title">
+						<?php  _oi("CURSOS"); ?>
+					</div>
+					<div class="text">
+						<?php  _oi("Vivamus porta aliquet ornare. Vivamus ac pharetra mi. Cras vel est tortor. Nullam non viverra magna, non finibus libero."); ?>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="box">
+					<div class="icon">
+						<img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/icon_software.png" />
+					</div>
+					<div class="title">
+						<?php  _oi("SOFTWARE"); ?>
+					</div>
+					<div class="text">
+						<?php  _oi("Vivamus porta aliquet ornare. Vivamus ac pharetra mi. Cras vel est tortor. Nullam non viverra magna, non finibus libero."); ?>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4 ">
+				<div class="box">
+					<div class="icon">
+						<img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/icon_rede.png" />
+					</div>
+					<div class="title">
+						<?php  _oi("REDE"); ?>
+					</div>
+					<div class="text">
+						<?php  _oi("Vivamus porta aliquet ornare. Vivamus ac pharetra mi. Cras vel est tortor. Nullam non viverra magna, non finibus libero."); ?>
+					</div>
+				</div>
+			</div>
+		</div> <!-- /.row -->
+	</div><!-- /.content -->
+</section>
+
+<section id="section2" class="">
+	<div class="container">
+		<div class="row image">
+			<div class="col-md-6 image">
+				<img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/icon_curso_soft.png" />
+			</div>
+			<div class="col-md-6 text">
+				<h2>
+					<?php  _oi("Cursos ipsum dolor sit"); ?>
+				</h2>
+				<p><?php  _oi("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas id velit lobortis, ultricies magna porta, molestie diam orem ipsum dolor sit amet porta."); ?></p>
+				<a href="#" class="btn_conheca"><?php  _oi("Conheca nossa plataforma"); ?></a>
+				<a href="#" class="btn_cadastrese"><?php  _oi("Cadastre-se"); ?></a>
+			</div>
+		</div>
+	</div><!-- /.content -->
+</section>
+<section id="section3" class="">
+	<div class="container">
+		<div class="col-md-6 image">
+			<img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/icon_soft_section.png" />
+		</div>
+		<div class="col-md-6 text">
+			<h2>
+				<?php  _oi("Software ipsum dolor sit"); ?>
+			</h2>
+			<p><?php  _oi("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas id velit lobortis, ultricies magna porta, molestie diam orem ipsum dolor sit amet porta."); ?></p>
+			<a href="#" class="btn_conheca"><?php  _oi("Conheca nossa documentacao"); ?></a>
+			<a href="#" class="btn_cadastrese"><?php  _oi("COMECE A TESTAR"); ?></a>
+		</div>
+		
+	</div><!-- /.content -->
+</section>
+<section id="section4" class="">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 image">
+				<img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/icon_rede_section.png" />
+			</div>
+			<div class="col-md-6 text">
+				<h2>
+					<?php  _oi("Rede ipsum dolor sit"); ?>
+				</h2>
+				<p><?php  _oi("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas id velit lobortis, ultricies magna porta, molestie diam orem ipsum dolor sit amet porta."); ?></p>
+				<a href="#" class="btn_conheca"><?php  _oi("Faca agora o download"); ?></a>
+				<a href="#" class="btn_cadastrese"><?php  _oi("COMECE A TESTAR"); ?></a>
+			</div>
+		</div>
+	</div><!-- /.content -->
+</section>
+<?php
+    do_action('get_footer');
+    get_template_part('templates/footer');
+    wp_footer();
+?>
+
