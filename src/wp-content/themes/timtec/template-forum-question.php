35acda979f8a53acdc5d96ee8ccf27cfbe361995
Leandro Monteiro dal Bó <leandro.db@gmail.com>
2015-11-26 19:09:15 -0200

    Template da question do fórum similar ao do fórum (refs #45)

--- src/wp-content/themes/timtec/template-forum-question.php
+++ src/wp-content/themes/timtec/template-forum-question.php
@@ -1,16 +1,13 @@
+<?php global $_MENUS; ?>
 <div class="wrap " role="document">
     <div class="container">
-        <main class="main" role="main">
-            <div id="page-forum" class="base-content dwqa-container">
-                <section>
-                    <div class="col-md-12 ">
-                        <h3><?php _oi("FORUM"); ?></h3>
-
-                        <?php get_template_part('templates/content-single', get_post_type()); ?>
-                    </div>
-                </section>
-            </div>
-        </main><!-- /.main -->
-        <div class="clear"></div>
+        <div class="page-header">
+            <h1>
+                <a href="<?php echo $_MENUS['rede']['Fórum']; ?>">
+                    <?php _oi("Fórum"); ?>
+                </a>
+            </h1>
+        </div>
+        <?php get_template_part('templates/content-single', get_post_type()); ?>
     </div>
 </div><!-- /.wrap -->
