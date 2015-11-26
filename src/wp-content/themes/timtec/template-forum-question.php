0ee1cc1f45d4cabb9b6985a0d7ed1443069aab94
Leandro Monteiro dal Bó <leandro.db@gmail.com>
2015-11-26 18:26:52 -0200

    Template forum question single incompleto (refs #45)

---
+++ src/wp-content/themes/timtec/template-forum-question.php
@@ -0,0 +1,16 @@
+<div class="wrap " role="document">
+    <div class="container">
+        <main class="main" role="main">
+            <div id="page-forum" class="base-content dwqa-container">
+                <section>
+                    <div class="col-md-12 ">
+                        <h3><?php _oi("FORUM"); ?></h3>
+
+                        <?php get_template_part('templates/content-single', get_post_type()); ?>
+                    </div>
+                </section>
+            </div>
+        </main><!-- /.main -->
+        <div class="clear"></div>
+    </div>
+</div><!-- /.wrap -->
