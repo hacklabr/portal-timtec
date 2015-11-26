a158b814cd8857b5ad802591d32b74074847d006
Leandro Monteiro dal Bó <leandro.db@gmail.com>
2015-11-26 18:45:08 -0200

    Config do Sage para remover sidebar na single do forum dwqa (refs #45)

--- src/wp-content/themes/timtec/lib/config.php
+++ src/wp-content/themes/timtec/lib/config.php
@@ -56,6 +56,7 @@ function display_sidebar() {
         'is_front_page',
         ['is_page_template', 'template-custom.php'],
         ['is_page_template', 'template-forum.php'],
+        ['is_singular', ['dwqa-question']],
         ['is_post_type_archive', ['faq']]
       ]
     );
