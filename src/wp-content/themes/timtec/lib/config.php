073f17668ef1f4a27f4efec9da4a361b938d20d3
Ariadne Melo <ariadnevmelo@gmail.com>
2015-11-27 12:44:26 -0200

    try to fix theme config

--- src/wp-content/themes/timtec/lib/config.php
+++ src/wp-content/themes/timtec/lib/config.php
@@ -54,7 +54,7 @@ function display_sidebar() {
       [
         'is_404',
         'is_front_page',
-        ['is_page_template'],
+        'is_page',
         ['is_singular', ['dwqa-question']],
         ['is_post_type_archive', ['faq']]
       ]
