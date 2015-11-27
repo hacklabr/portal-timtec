90052bd49a10fa237a2fc1b429cf1f3ecf585982
Ariadne Melo <ariadnevmelo@gmail.com>
2015-11-27 12:39:28 -0200

    add the wrappers to single template

--- src/wp-content/themes/timtec/single.php
+++ src/wp-content/themes/timtec/single.php
@@ -1,1 +1,7 @@
-<?php get_template_part('templates/content-single', get_post_type()); ?>
+<div class="base-content container">
+    <section>
+        <div class="col-md-12 ">
+            <?php get_template_part('templates/content-single', get_post_type()); ?>
+        </div>
+    </section>
+</div>
