c7a71711f9dd800e2e64deccfa799bcc13cbc3ec
Ariadne Melo <ariadnevmelo@gmail.com>
2015-12-17 17:56:21 -0200

    notícias em destaque e lista de notícias (refs #61)

--- src/wp-content/themes/timtec/page-news.php
+++ src/wp-content/themes/timtec/page-news.php
@@ -13,7 +13,7 @@ get_template_part('templates/header');
         <div class="container">
             <h2 class="title"><?php _oi("Novidades"); ?></h2>
             <div class="info">
-                <span class="post-category">#Categoria</span>
+                <span class="post-category orange">#Categoria</span>
                 <h3 class="post-title"><a href="">Lorem ipsum dolor sit amet consectetur adipiscing elit</a></h3>
                 <div class="post-excerpt"><a href="">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis molestie a dui at porttitor. Nunc at imperdiet ipsum, in consequat elit.</a></div>
                 <div class="read-more"><a href=""><?php _oi("Leia mais"); ?> <span class="arrow"><i class="fa fa-arrow-right"></i></span></a></div>
@@ -22,8 +22,58 @@ get_template_part('templates/header');
     </div>
 
     <div class="container">
-        <section>
+        <div class="row">
+            <div class="news col-md-8">
+                <div class="featured row">
+                    <div class="featured-big">
+                        <div class="news-box" style="background-image: url(http://lorempixel.com/600/400/);">
+                            <span class="post-category blue">#Categoria</span>
+                            <h3 class="post-title"><a href="">Lorem ipsum dolor sit amet consectetur adipiscing elit</a></h3>
+                            <div class="post-excerpt"><a href="">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis molestie a dui at porttitor. Nunc at imperdiet ipsum, in consequat elit.</a></div>
+                        </div>
+                    </div>
+                    <div class="featured-small">
+                        <div class="news-box" style="background-image: url(http://lorempixel.com/600/400/);">
+                            <span class="post-category blue">#Categoria</span>
+                            <h3 class="post-title"><a href="">Lorem ipsum dolor sit amet consectetur adipiscing elit</a></h3>
+                        </div>
+                    </div>
+                    <div class="featured-small">
+                        <div class="news-box" style="background-image: url(http://lorempixel.com/600/400/);">
+                            <span class="post-category blue">#Categoria</span>
+                            <h3 class="post-title"><a href="">Lorem ipsum dolor sit amet consectetur adipiscing elit</a></h3>
+                        </div>
+                    </div>
+                </div>
 
-        </section>
+                <div class="list">
+                    <h2 class="list-title">Notícias geral</h2>
+
+                    <div class="list-item">
+                        <span class="post-category orange">#Categoria</span>
+                        <time class="post-date">as 00h00 dia 00/00/0000</time>
+                        <h3 class="post-title"><a href="">Lorem ipsum dolor sit amet consectetur adipiscing elit</a></h3>
+                        <div class="post-excerpt"><a href="">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis molestie a dui at porttitor. Nunc at imperdiet ipsum, in consequat elit.</a></div>
+                    </div>
+
+                    <div class="list-item">
+                        <span class="post-category orange">#Categoria</span>
+                        <time class="post-date">as 00h00 dia 00/00/0000</time>
+                        <h3 class="post-title"><a href="">Lorem ipsum dolor sit amet consectetur adipiscing elit</a></h3>
+                        <div class="post-excerpt"><a href="">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis molestie a dui at porttitor. Nunc at imperdiet ipsum, in consequat elit.</a></div>
+                    </div>
+
+                    <div class="list-item">
+                        <span class="post-category orange">#Categoria</span>
+                        <time class="post-date">as 00h00 dia 00/00/0000</time>
+                        <h3 class="post-title"><a href="">Lorem ipsum dolor sit amet consectetur adipiscing elit</a></h3>
+                        <div class="post-excerpt"><a href="">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis molestie a dui at porttitor. Nunc at imperdiet ipsum, in consequat elit.</a></div>
+                    </div>
+                </div>
+            </div>
+            <div class="sidebar col-md-4">
+                
+            </div>
+        </div>
     </div>
 </div>
