5c7e68065b16ad4f657b15d8ffbdd9d7e1d441cb
Ariadne Melo <ariadnevmelo@gmail.com>
2015-12-18 15:03:00 -0200

    estilos da página sobre (refs #62)

--- src/wp-content/themes/timtec/page-about.php
+++ src/wp-content/themes/timtec/page-about.php
@@ -13,14 +13,64 @@ get_template_part('templates/header');
         <div class="container">
             <h2 class="title"><?php _oi("Sobre"); ?> <span class="subtitle">[<?php _oi("Nome da página"); ?>]</span></h2>
             <div class="info">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eleifend ex nec odio ultricies ornare. Ut facilisis tellus sit amet turpis sagittis, vitae pretium risus vulputate. Cras dolor ligula, aliquam in convallis non, ullamcorper eget risus. Sed malesuada lacus nec libero tempor viverra.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eleifend ex nec odio ultricies ornare. Ut facilisis tellus sit amet turpis sagittis, vitae pretium risus vulputate. Cras dolor ligula, aliquam in convallis non, ullamcorper eget risus. Sed malesuada lacus nec libero tempor viverra. 
             </div>
         </div>
     </div>
 
-    <div class="container">
+    <section class="page-content">
+        <div class="container">
 
+            <h2 class="title">Subtítulo</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eleifend ex nec odio ultricies ornare. Ut facilisis tellus sit amet turpis sagittis, vitae pretium risus vulputate. Cras dolor ligula, aliquam in convallis non, ullamcorper eget risus. Sed malesuada lacus nec libero tempor viverra.</p>
 
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eleifend ex nec odio ultricies ornare. Ut facilisis tellus sit amet turpis sagittis, vitae pretium risus vulputate. Cras dolor ligula, aliquam in convallis non, ullamcorper eget risus. Sed malesuada lacus nec libero tempor viverra.</p>
 
-    </div>
+            <p class="featured-content">"Lorem ipsum dolor sit amet"</p>
+
+            <h2 class="title">Subtítulo</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eleifend ex nec odio ultricies ornare. Ut facilisis tellus sit amet turpis sagittis, vitae pretium risus vulputate. Cras dolor ligula, aliquam in convallis non, ullamcorper eget risus. Sed malesuada lacus nec libero tempor viverra.</p>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eleifend ex nec odio ultricies ornare. Ut facilisis tellus sit amet turpis sagittis, vitae pretium risus vulputate. Cras dolor ligula, aliquam in convallis non, ullamcorper eget risus. Sed malesuada lacus nec libero tempor viverra.</p>
+        </div>
+    </section>
+
+    <section class="page-featured">
+        <h2 class="title">lorem ipsum dolor sit amet</h2>
+        <div class="featured-line">
+            <div class="row">
+                <div class="container">
+                    <div class="featured-box">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icones-1.png" alt="1">
+                        <h3 class="subtitle">Subtítulo</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eleifend ex nec odio ultricies ornare. Ut facilisis tellus sit amet turpis sagittis, vitae pretium risus vulputate. Cras dolor ligula, aliquam in convallis non, ullamcorper eget risus. Sed malesuada lacus nec libero tempor viverra.</p>
+                    </div>
+                    <div class="featured-box">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icones-2.png" alt="2">
+                        <h3 class="subtitle">Subtítulo</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eleifend ex nec odio ultricies ornare. Ut facilisis tellus sit amet turpis sagittis, vitae pretium risus vulputate. Cras dolor ligula, aliquam in convallis non, ullamcorper eget risus. Sed malesuada lacus nec libero tempor viverra.</p>
+                    </div>
+                    <div class="featured-box">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icones-3.png" alt="3">
+                        <h3 class="subtitle">Subtítulo</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eleifend ex nec odio ultricies ornare. Ut facilisis tellus sit amet turpis sagittis, vitae pretium risus vulputate. Cras dolor ligula, aliquam in convallis non, ullamcorper eget risus. Sed malesuada lacus nec libero tempor viverra.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="page-content">
+        <div class="container">
+            <h2 class="title">Subtítulo</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eleifend ex nec odio ultricies ornare. Ut facilisis tellus sit amet turpis sagittis, vitae pretium risus vulputate. Cras dolor ligula, aliquam in convallis non, ullamcorper eget risus. Sed malesuada lacus nec libero tempor viverra.</p>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eleifend ex nec odio ultricies ornare. Ut facilisis tellus sit amet turpis sagittis, vitae pretium risus vulputate. Cras dolor ligula, aliquam in convallis non, ullamcorper eget risus. Sed malesuada lacus nec libero tempor viverra.</p>
+
+            <h2 class="title">Subtítulo</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eleifend ex nec odio ultricies ornare. Ut facilisis tellus sit amet turpis sagittis, vitae pretium risus vulputate. Cras dolor ligula, aliquam in convallis non, ullamcorper eget risus. Sed malesuada lacus nec libero tempor viverra.</p>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eleifend ex nec odio ultricies ornare. Ut facilisis tellus sit amet turpis sagittis, vitae pretium risus vulputate. Cras dolor ligula, aliquam in convallis non, ullamcorper eget risus. Sed malesuada lacus nec libero tempor viverra.</p>
+        </div>
+    </section>
 </div>
