30dcacfb150c9cd91087e8740c46d9b06ab32762
Ariadne Melo <ariadnevmelo@gmail.com>
2015-12-18 12:51:38 -0200

    template e imagens da página sobre (#62)

---
+++ src/wp-content/themes/timtec/page-about.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Template Name: Sobre
+ */
+?>
+
+<?php
+do_action('get_header');
+get_template_part('templates/header');
+?>
+<div id="page-about" class="base-content">
+    <div class="banner">
+        <div class="container">
+            <h2 class="title"><?php _oi("Sobre"); ?> <span class="subtitle">[<?php _oi("Nome da página"); ?>]</span></h2>
+            <div class="info">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eleifend ex nec odio ultricies ornare. Ut facilisis tellus sit amet turpis sagittis, vitae pretium risus vulputate. Cras dolor ligula, aliquam in convallis non, ullamcorper eget risus. Sed malesuada lacus nec libero tempor viverra.
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+
+
+
+    </div>
+</div>
