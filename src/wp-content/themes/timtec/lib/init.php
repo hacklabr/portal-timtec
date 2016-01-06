042fe3c40e0ab99273a5896d5863aa948f170013
Ariadne Melo <ariadnevmelo@gmail.com>
2016-01-06 09:54:37 -0200

    arruma layout dos widgets

--- src/wp-content/themes/timtec/lib/init.php
+++ src/wp-content/themes/timtec/lib/init.php
@@ -64,8 +64,8 @@ function widgets_init() {
     'id'            => 'sidebar-primary',
     'before_widget' => '<section class="widget %1$s %2$s">',
     'after_widget'  => '</section>',
-    'before_title'  => '<h3>',
-    'after_title'   => '</h3>'
+    'before_title'  => '<h2 class="sidebar-title">',
+    'after_title'   => '</h2>'
   ]);
 
   register_sidebar([
