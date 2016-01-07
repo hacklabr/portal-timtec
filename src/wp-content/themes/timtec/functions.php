cf65e0b7b281462364fd8544f44fd90f2aaafd3c
ElvisBM <elvisbmartins@gmail.com>
2016-01-07 17:51:55 -0200

    Remove botões de rede social do JetPack das pages de noticias(list category) e forum(list category)

--- src/wp-content/themes/timtec/functions.php
+++ src/wp-content/themes/timtec/functions.php
@@ -298,3 +298,18 @@ function rename_post_for_noticia_object() {
   $labels->menu_name = 'Notícias';
   $labels->name_admin_bar = 'Notícias';
 }
+
+
+
+// Remove sharing buttons on archive and search pages
+function jptweak_remove_share() {
+  if ( is_page( 4, 46  ) ) {
+      remove_filter( 'the_content', 'sharing_display',19 );
+      remove_filter( 'the_excerpt', 'sharing_display',19 );
+      if ( class_exists( 'Jetpack_Likes' ) ) {
+          remove_filter( 'the_content', array( Jetpack_Likes::init(), 'post_likes' ), 30, 1 );
+      }
+  }
+
+}
+add_action( 'loop_start', 'jptweak_remove_share' );
