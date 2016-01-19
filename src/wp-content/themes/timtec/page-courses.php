30ff2ad701cf4c07ca77c5d0d5653b77745b5272
Ariadne Melo <ariadnevmelo@gmail.com>
2016-01-19 12:55:11 -0200

    coloca título do slider do carousel no mobile (refs #80)

--- src/wp-content/themes/timtec/page-courses.php
+++ src/wp-content/themes/timtec/page-courses.php
@@ -19,13 +19,15 @@ get_template_part('templates/header');
                 <!-- Wrapper for slides -->
                 <div class="carousel-inner" role="listbox">
                     <div class="item active">
+                        <div class="mobile-text">1. <?php _oi("Introdução"); ?></div>
                         Aqui você encontra todos os cursos já produzidos e publicados pelo projeto TIM Tec ou por seus parceiros. Eles podem ser acessados, cursados e baixados por qualquer pessoa, gratuitamente. Cada curso é dividido em aulas e cada aula é dividida em capítulos de até 5 minutos. O aluno assiste ao vídeo, faz as atividades correspondentes e pode consultar o material complementar disponível para cada curso
                     </div>
                     <div class="item">
+                        <div class="mobile-text">2. <?php _oi("Eixos"); ?></div>
                         Os cursos são voltados para a área de tecnologia e foram escolhidos com base no Eixo Tecnológico: Informação e Comunicação do Programa Nacional de Acesso ao Ensino Técnico e Emprego (Pronatec), do governo federal. Há também cursos dirigidos a professores do Ensino Fundamental e ao fortalecimento de competências consideradas básicas para a formação de qualquer profissional, como a escrita de textos.
                     </div>
                     <div class="item">
-                        Os vídeos produzidos pelo projeto TIM Tec estão publicados sob a Licença Creative Commons Atribuição 3.0 Brasil (CC-BY), a mais livre de todas. Os conteúdos podem ser livremente compartilhados (copiados e redistribuídos em qualquer suporte ou formato) e adaptados (remixados, transformados e usados como base para outros materiais), desde que seja dado o devido crédito ao projeto TIM Tec e ao Instituto TIM.
+                        <div class="mobile-text">3. <?php _oi("Licença"); ?></div> Os vídeos produzidos pelo projeto TIM Tec estão publicados sob a Licença Creative Commons Atribuição 3.0 Brasil (CC-BY), a mais livre de todas. Os conteúdos podem ser livremente compartilhados (copiados e redistribuídos em qualquer suporte ou formato) e adaptados (remixados, transformados e usados como base para outros materiais), desde que seja dado o devido crédito ao projeto TIM Tec e ao Instituto TIM.
                     </div>
                 </div>
 
