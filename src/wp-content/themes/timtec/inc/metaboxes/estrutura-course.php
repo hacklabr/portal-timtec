9ade962fccd9128e0a1f76ecdee6a7eaa2d95cc5
ElvisBM <elvisbmartins@gmail.com>
2016-01-26 17:42:10 -0200

    Curso novos campos(Estrutura, Nivel, Pre-requisito, Qtd aulas, Qtd Horas) , (refs #96)

---
+++ src/wp-content/themes/timtec/inc/metaboxes/estrutura-course.php
@@ -0,0 +1,79 @@
+<?php
+
+class add_estrutura_course{
+
+    protected static $metabox_config = array(
+        'add_estrutura_course',
+        'Estrutura do Curso',
+        'course',
+        'normal'
+    );
+
+    static function init() {
+        add_action('add_meta_boxes', array(__CLASS__, 'addMetaBox'));
+        add_action('save_post', array(__CLASS__, 'savePost'));
+    }
+
+    static function addMetaBox() {
+        add_meta_box(
+            self::$metabox_config[0],
+            self::$metabox_config[1],
+            array(__CLASS__, 'metabox'),
+            self::$metabox_config[2],
+            self::$metabox_config[3]
+
+        );
+    }
+
+    static function metabox(){
+        global $post;
+
+        wp_nonce_field( 'save_'.__CLASS__, __CLASS__.'_noncename' );
+
+        $metadata = get_metadata('post', $post->ID);
+        if(!isset($metadata['estrutura_course'])){
+           $content = "";
+        }else{
+            $content = $metadata['estrutura_course'][0];
+        };
+        ?>
+        <textarea name="<?php echo __CLASS__ ?>[estrutura_course]" style="background-color: #fff; font-size: 1.7em; height: 4em; line-height: 100%; margin: 0; outline: 0; padding: 3px 8px; width: 100%;">
+        	<?php echo $content; ?>
+        </textarea>
+        <?php
+    }
+
+    static function savePost($post_id) {
+        // verify if this is an auto save routine.
+        // If it is our form has not been submitted, so we dont want to do anything
+        if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE)
+            return;
+
+        // verify this came from the our screen and with proper authorization,
+        // because save_post can be triggered at other times
+        if(!isset($_POST[__CLASS__.'_noncename']))
+            return;
+        if (!wp_verify_nonce($_POST[__CLASS__.'_noncename'], 'save_'.__CLASS__))
+            return;
+
+
+        // Check permissions
+        if ('page' == $_POST['post_type']) {
+            if (!current_user_can('edit_page', $post_id))
+                return;
+        }
+        else {
+            if (!current_user_can('edit_post', $post_id))
+                return;
+        }
+
+        // OK, we're authenticated: we need to find and save the data
+        if(isset($_POST[__CLASS__])){
+            foreach($_POST[__CLASS__] as $meta_key => $value)
+                update_post_meta($post_id, $meta_key, $value);
+        }
+    }
+
+
+}
+add_estrutura_course::init();
