b9984348276c7fb3850b9225e8c05662fbe78e1a
Ariadne Melo <ariadnevmelo@gmail.com>
2016-01-27 18:13:42 -0200

    ajustes na página de notícias (refs #102)

--- src/wp-content/themes/timtec/single.php
+++ src/wp-content/themes/timtec/single.php
@@ -30,7 +30,7 @@ get_template_part('templates/header');
 <div id="page-news" class="base-content page single">
     <div class="banner">
         <div class="container">
-            <h2 class="title"><?php _oi("Novidades"); ?></h2>
+            <h2 class="title"><?php _oi("Notícias"); ?></h2>
         </div>
     </div>
     <?php get_template_part('templates/content-single', get_post_type()); ?>
